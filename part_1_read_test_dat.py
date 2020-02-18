29a040ebf26d77a50e16a7cb7b4d61ade80d6475
KBTHIES <kbthies@gmail.com>
2020-02-18 17:51:36 -0500

    phase 2
    added test_data.json, restructured part 1 to be in line with the comments, and filled in phase 2

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -2,10 +2,9 @@ import cc_dat_utils
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
-from cc_dat_utils import make_cc_level_pack_from_dat
-print(make_cc_level_pack_from_dat("data/pfgd_test.dat"))
-
-
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+from cc_dat_utils import make_cc_level_pack_from_dat
+
 #print the resulting data
+print(make_cc_level_pack_from_dat("data/pfgd_test.dat"))
--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -5,14 +5,19 @@ import json
 def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
-
     ### Begin Add Code Here ###
     #Loop through the json_data
+    for x in json_data:
         #Create a new Game object from the json_data by reading
         #  title
+        title = x['title']
         #  year
+        year = x['year']
         #  platform (which requires reading name and launch_year)
+        platform = test_data.Platform(x['platform']['name'], x['platform']['launch year'])
+        game = test_data.Game(title, platform, year)
         #Add that Game object to the game_library
+        game_library.add_game(game)
     ### End Add Code Here ###
 
     return game_library
@@ -23,7 +28,10 @@ input_json_file = "data/test_data.json"
 
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
+with open("data/test_data.json", "r") as reader:
 #Use the json module to load the data from the file
+    json_data = json.load(reader)
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
+print(make_game_library_from_json(json_data))
 #Print out the resulting GameLibrary data using print()
 ### End Add Code Here ###
